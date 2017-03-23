cfa7a2568235289869322ffd80dc6d97e64f241d
Louis Tur <louis.tur@gmail.com>
2017-03-23 19:44:25 -0400

    base proj commit

---
+++ CI-Comparisons/CI-Comparisons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CI-Comparisons
+//
+//  Created by Louis Tur on 3/23/17.
+//  Copyright © 2017 AccessCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
